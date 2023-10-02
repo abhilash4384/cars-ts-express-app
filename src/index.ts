6e89e9fc32f752fc0cc963123a46056921a1a537
techiabhil <techiabhil@gmail.com>
2023-10-02 20:49:05 +0530

    Error logger & default route implemented

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 // followed this blog https://blog.logrocket.com/how-to-set-up-node-typescript-express/
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import helmet from 'helmet';
 import path from 'path';
+import errorLogger from './middleware/error-logger';
 import authRoutes from './routes/auth-routes';
-
 dotenv.config();
 const app = express();
+
+app.use(cors());
+app.use(helmet());
 app.use(express.json());
 
-console.log(path.join(__dirname, process.env.PICS_DIR ?? ''));
 app.use(
   '/pics/',
   express.static(path.join(__dirname, process.env.PICS_DIR ?? ''))
@@ -16,10 +20,22 @@ app.use(
 app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Express - TypeScript Server healthcheck');
-  res.end();
+  res.status(200).json({
+    isSuccess: true,
+    data: null,
+    message: 'Express - TypeScript Server healthcheck',
+  });
 });
 
+app.use((_, res) => {
+  res.status(404).json({
+    isSuccess: false,
+    data: null,
+    message: 'Resource not found!',
+  });
+});
+
+app.use(errorLogger);
 const PORT = process.env.PORT ?? 4001;
 app.listen(PORT, () => {
   console.log(`💻Server listening on http://localhost:${PORT}`);
---
+++ src/middleware/error-logger.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from 'express';
+import fs from 'fs';
+export default function (
+  err: Error,
+  req: Request,
+  res: Response,
+  _: NextFunction
+) {
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.toLocaleString('default', { month: 'long' });
+  const year = date.getFullYear();
+
+  // create log files date wise
+  const fileName = `${day}-${month}-${year}-error-logs.txt`;
+
+  console.log('\n Error = ', err);
+  let errorTxt: any = err;
+  if (typeof err !== 'string') {
+    errorTxt = err?.toString();
+  }
+  const currentDate = new Date().toString();
+  const stars = '*'.repeat(10);
+  fs.appendFile(
+    `./logs/${fileName}`,
+    `\n${stars} Start Log: ${currentDate} ${stars}\n ${errorTxt} \n`,
+    (fileError) => {
+      if (fileError) console.log('Error while writing logs ');
+    }
+  );
+
+  res.status(500).json({
+    isSuccess: false,
+    data: null,
+    message: 'Something went wrong',
+  });
+}
